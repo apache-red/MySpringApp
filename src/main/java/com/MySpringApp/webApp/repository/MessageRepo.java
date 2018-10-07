496667fda6333103d764bb9aa696e38742d66009
apache-red <apache_red@mail.ru>
2018-10-07 19:35:17 +0300

    the project was added

---
+++ src/main/java/com/MySpringApp/webApp/repository/MessageRepo.java
@@ -0,0 +1,13 @@
+package com.MySpringApp.webApp.repository;
+
+import com.MySpringApp.webApp.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List <Message> findByTag(String tag);
+
+
+}
